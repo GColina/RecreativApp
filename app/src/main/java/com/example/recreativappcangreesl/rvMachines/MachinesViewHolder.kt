5d66e3604466eced393916b8b7b14217a3fcbe36
GColina <gcolinaramirez@gmail.com>
2024-02-23 23:42:33 +0100

    Gestion no acabada de la pantalla detail

--- app/src/main/java/com/example/recreativappcangreesl/rvMachines/MachinesViewHolder.kt
+++ app/src/main/java/com/example/recreativappcangreesl/rvMachines/MachinesViewHolder.kt
@@ -7,18 +7,19 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recreativappcangreesl.R
 
-class MachinesViewHolder(view: View) : RecyclerView.ViewHolder(view){
 
-    private val tvBarName:TextView = view.findViewById(R.id.tvBarName)
-    private val ivMachine:ImageView =  view.findViewById(R.id.ivMachine)
-    private val ivNotes:ImageView =  view.findViewById(R.id.ivNotes)
-    private val ivLocation:ImageView =  view.findViewById(R.id.ivLocation)
-    private val ivAlertTime:ImageView =  view.findViewById(R.id.ivAlertTime)
-    private val tvOwnerName:TextView = view.findViewById(R.id.tvOwnerName)
-    private val tvOwnerPhone:TextView = view.findViewById(R.id.tvOwnerPhone)
-    private val cvCategories:CardView = view.findViewById(R.id.cvCategories)
+class MachinesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
-    fun render(machine: Machine){
+    private val tvBarName: TextView = view.findViewById(R.id.tvBarName)
+    private val ivMachine: ImageView = view.findViewById(R.id.ivAsset)
+    private val ivNotes: ImageView = view.findViewById(R.id.ivNotes)
+    private val ivLocation: ImageView = view.findViewById(R.id.ivLocation)
+    private val ivAlertTime: ImageView = view.findViewById(R.id.ivAlertTime)
+    private val tvOwnerName: TextView = view.findViewById(R.id.tvOwnerName)
+    private val tvOwnerPhone: TextView = view.findViewById(R.id.tvOwnerPhone)
+    private val cvCategories: CardView = view.findViewById(R.id.cvCategories)
+
+    fun render(machine: Machine) {
         tvBarName.text = machine.BarName
     }
 }
